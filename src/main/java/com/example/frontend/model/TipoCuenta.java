08511e5b18b9d0240fe4a5ccbb469ac9257dd317
morenojacomedanna-sys <morenojacomedanna@gmail.com>
2026-07-08 20:57:55 -0500

    agregegando los enums: TipoCuenta y TipoTransaccion en el package model

---
+++ src/main/java/com/example/frontend/model/TipoCuenta.java
@@ -0,0 +1,6 @@
+package com.example.frontend.model;
+
+public enum TipoCuenta {
+    AHORROS,
+    CORRIENTE
+}
---
+++ src/main/java/com/example/frontend/model/TipoTransaccion.java
@@ -0,0 +1,7 @@
+package com.example.frontend.model;
+
+public enum TipoTransaccion {
+    DEPOSITO,
+    RETIRO,
+    TRANSFERENCIA
+}
